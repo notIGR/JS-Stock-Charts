53be9603353944c13ab4574a6f1195bccaed48b2
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-05-11 15:56:17 -0400

    arrow function and api KEY

--- public/index.js
+++ public/index.js
@@ -1,9 +1,11 @@
-async function main() {
-
-    const timeChartCanvas = document.querySelector('#time-chart');
-    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
-    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
+const main = () => {
+  const timeChartCanvas = document.querySelector("#time-chart");
+  const highestPriceChartCanvas = document.querySelector(
+    "#highest-price-chart"
+  );
+  const averagePriceChartCanvas = document.querySelector(
+    "#average-price-chart"
+  );
 }
 
-main() 
+main(fea75586ff664876bc76e2f7556f6387);
