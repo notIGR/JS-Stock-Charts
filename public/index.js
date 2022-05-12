96cbdfbac9b7f5902199f04575ad4a6eb55a3152
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-05-12 13:38:37 -0400

    defined colors for each stock

--- public/index.js
+++ public/index.js
@@ -1,31 +1,47 @@
 const main = async () => {
-    const timeChartCanvas = document.querySelector("#time-chart");
-    const highestPriceChartCanvas = document.querySelector("#highest-price-chart");
-    const averagePriceChartCanvas = document.querySelector(  "#average-price-chart");
+  const timeChartCanvas = document.querySelector("#time-chart");
+  const highestPriceChartCanvas = document.querySelector(
+    "#highest-price-chart"
+  );
+  const averagePriceChartCanvas = document.querySelector(
+    "#average-price-chart"
+  );
 
-    const response = await fetch("https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=fea75586ff664876bc76e2f7556f6387");
-    const data = await response.json()
-    console.log(data)
-    const {GME,MSFT,DIS,BNTX} = data;
-    const stocks = [GME,MSFT,DIS,BNTX];
-    stocks.forEach( stock => stock.values.reverse())
+  const response = await fetch(
+    "https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=fea75586ff664876bc76e2f7556f6387"
+  );
+  const data = await response.json();
+  console.log(data);
+  const { GME, MSFT, DIS, BNTX } = data;
+  const stocks = [GME, MSFT, DIS, BNTX];
+  stocks.forEach((stock) => stock.values.reverse());
 
-    new Chart(timeChartCanvas.getContext('2d'), {
-      type: 'line',
-      data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor:  'rgba(255, 99, 132, 0.2)',
-              borderColor: 'rgba(255, 99, 132, 1)'
-          }]
-      }
+  new Chart(timeChartCanvas.getContext("2d"), {
+    type: "line",
+    data: {
+      labels: stocks[0].values.map((value) => value.datetime),
+      datasets: stocks.map((stock) => ({
+        label: stock.meta.symbol,
+        data: stock.values.map((value) => parseFloat(value.high)),
+        backgroundColor: getColor(stock.meta.symbol),
+        borderColor: getColor(stock.meta.symbol),
+      })),
+    },
   });
-  };
-  
-
-
-
+};
+function getColor(stock) {
+  if (stock === "GME") {
+    return "rgba(61, 161, 61, 0.7)";
+  }
+  if (stock === "MSFT") {
+    return "rgba(209, 4, 25, 0.7)";
+  }
+  if (stock === "DIS") {
+    return "rgba(18, 4, 209, 0.7)";
+  }
+  if (stock === "BNTX") {
+    return "rgba(166, 43, 158, 0.7)";
+  }
+}
 
 main();
