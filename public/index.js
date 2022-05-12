fcdd28035902fbe58c142935a124ddab5c4e198e
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-05-12 13:34:17 -0400

    got line time chart functional, did not work at first but it was becasue I did not have it scoped correctly)

--- public/index.js
+++ public/index.js
@@ -1,11 +1,31 @@
-const main = () => {
-  const timeChartCanvas = document.querySelector("#time-chart");
-  const highestPriceChartCanvas = document.querySelector(
-    "#highest-price-chart"
-  );
-  const averagePriceChartCanvas = document.querySelector(
-    "#average-price-chart"
-  );
-}
+const main = async () => {
+    const timeChartCanvas = document.querySelector("#time-chart");
+    const highestPriceChartCanvas = document.querySelector("#highest-price-chart");
+    const averagePriceChartCanvas = document.querySelector(  "#average-price-chart");
 
-main(fea75586ff664876bc76e2f7556f6387);
+    const response = await fetch("https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=fea75586ff664876bc76e2f7556f6387");
+    const data = await response.json()
+    console.log(data)
+    const {GME,MSFT,DIS,BNTX} = data;
+    const stocks = [GME,MSFT,DIS,BNTX];
+    stocks.forEach( stock => stock.values.reverse())
+
+    new Chart(timeChartCanvas.getContext('2d'), {
+      type: 'line',
+      data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor:  'rgba(255, 99, 132, 0.2)',
+              borderColor: 'rgba(255, 99, 132, 1)'
+          }]
+      }
+  });
+  };
+  
+
+
+
+
+main();
